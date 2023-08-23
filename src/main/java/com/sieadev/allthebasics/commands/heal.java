5c4a3656b51417a996f8af127a5c7685be3d1fac
SIEA <69807609+DrVosss@users.noreply.github.com>
2023-08-23 22:08:20 +0200

    Updated plugin.yml Added /heal Added healPlayer function

--- src/main/java/com/sieadev/allthebasics/commands/heal.java
+++ src/main/java/com/sieadev/allthebasics/commands/heal.java
@@ -1,4 +1,23 @@
 package com.sieadev.allthebasics.commands;
 
-public class heal {
+import com.sieadev.allthebasics.util.healPlayer;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+public class heal implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (!(sender instanceof Player)){
+            sender.sendMessage("§cYou can not use this command in the Console.");
+        }
+        Player p = (Player) sender;
+        if (args.length > 0){
+            String target = args[0];
+            healPlayer.heal(p, target);
+            return true;
+        }
+        healPlayer.heal(p, null);
+        return true;
+    }
 }
---
+++ src/main/java/com/sieadev/allthebasics/util/healPlayer.java
@@ -0,0 +1,40 @@
+package com.sieadev.allthebasics.util;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+
+public class healPlayer {
+    public static void heal(Player p, String t){
+        if (!(p.hasPermission("atb.healPlayer"))){
+            p.sendMessage("§cYou don't have the required permissions to use this command.");
+            return;
+        }
+
+        if (t == null){
+            double missingHealth = p.getMaxHealth() - p.getHealth();
+            if (missingHealth == 0){
+                p.sendMessage(ChatColor.YELLOW + "You are already at maximum health: " + "§c" + p.getHealth() + "§c❤");
+                return;
+            }
+            p.sendMessage(ChatColor.YELLOW + "You healed yourself for §c" + Math.round(missingHealth) + "§c❤");
+            return;
+        }
+
+        Player target = getPlayerFromString.getPlayerFromString(p, t);
+
+        if (target == null){
+            return;
+        }
+
+        double missingHealth = (target.getMaxHealth() - target.getHealth());
+
+        if (missingHealth == 0){
+            p.sendMessage(ChatColor.YELLOW + "This Player is already at maximum health: " + "§c" + p.getHealth() + "§c❤");
+            return;
+        }
+
+        target.setHealth(target.getMaxHealth());
+        p.sendMessage(ChatColor.YELLOW + "You healed " + target.getDisplayName() + ChatColor.YELLOW + " for §c" + Math.round(missingHealth) + "§c❤");
+        target.sendMessage(p.getDisplayName() + ChatColor.YELLOW + " healed you for §c" + Math.round(missingHealth) + "§c❤");
+    }
+}
