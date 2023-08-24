2e2c67fe215c9c507cebd431fd65209a879a8051
Arda Aksu <79632956+aarda55@users.noreply.github.com>
2023-08-24 23:22:56 +0200

    Update AllTheBasics.java

--- src/main/java/com/sieadev/allthebasics/AllTheBasics.java
+++ src/main/java/com/sieadev/allthebasics/AllTheBasics.java
@@ -5,11 +5,13 @@ import org.bukkit.plugin.java.JavaPlugin;
 public final class AllTheBasics extends JavaPlugin {
 
 
-
     @Override
     public void onEnable() {
         sendConsoleMessage("Attempting to start ATB");
-
+        getCommand("gm").setExecutor(new gm0);
+        getCommand("gm").setExecutor(new gm1);
+        getCommand("gm").setExecutor(new gm2);
+        getCommand("gm").setExecutor(new gm3);
     }
 
     @Override
