434fb14e9db5f159af829392ae0076883c87539d
SIEA <69807609+DrVosss@users.noreply.github.com>
2023-08-30 00:33:25 +0200

    Added Intelligent Error handling

--- src/main/java/com/sieadev/allthebasics/AllTheBasics.java
+++ src/main/java/com/sieadev/allthebasics/AllTheBasics.java
@@ -2,34 +2,67 @@ package com.sieadev.allthebasics;
 
 import com.sieadev.allthebasics.commands.*;
 import com.sieadev.allthebasics.commands.gamemode.*;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class AllTheBasics extends JavaPlugin {
 
 
+    private int errors;
+
     @Override
     public void onEnable() {
-        sendConsoleMessage("Attempting to start ATB");
-        getCommand("tp").setExecutor(new tp());
-        getCommand("heal").setExecutor(new heal());
-        getCommand("feed").setExecutor(new feed());
-        getCommand("fakeop").setExecutor(new fakeop());
-        getCommand("fly").setExecutor(new fly());
-        getCommand("gm0").setExecutor(new gm0());
-        getCommand("gm1").setExecutor(new gm1());
-        getCommand("gm2").setExecutor(new gm2());
-        getCommand("gm3").setExecutor(new gm3());
-        getCommand("gm").setExecutor(new gm());
+        this.errors = 0;
+
+        try {
+            sendConsoleMessage("Attempting to start ATB...");
+
+            sendConsoleMessage("Successfully loaded the Config");
+        } catch (Exception e) {
+            // Handle the exception here
+            System.err.println("An error occurred when loading the Config: " + e.getMessage());
+            this.errors = errors + 1;
+        }
+
+        try {
+            getCommand("tp").setExecutor(new tp());
+            getCommand("heal").setExecutor(new heal());
+            getCommand("feed").setExecutor(new feed());
+            getCommand("fakeop").setExecutor(new fakeop());
+            getCommand("fly").setExecutor(new fly());
+            getCommand("gm0").setExecutor(new gm0());
+            getCommand("gm1").setExecutor(new gm1());
+            getCommand("gm2").setExecutor(new gm2());
+            getCommand("gm3").setExecutor(new gm3());
+            getCommand("gm").setExecutor(new gm());
+        } catch (Exception e) {
+            System.err.println("An error occurred when loading in Commands: " + e.getMessage());
+            this.errors = errors + 1;
+        }
+
+        if (errors < 1) {
+            sendConsoleMessage("ATB Successfully started");
+        }
+        else
+        {
+            sendConsoleMessage(ChatColor.RED + "ATB started with one or more errors: " + errors + " errors" );
+            sendConsoleMessage(ChatColor.RED + "We would appreciate you reporting this to the official GitHub https://github.com/sieadev/allTheBasics" );
+        }
+
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        sendConsoleMessage("Shutting down ATB...");
     }
 
     private void sendConsoleMessage(String message) {
         getServer().getConsoleSender().sendMessage(message);
     }
 
+
+    private void loadConfig(){
+
+    }
 
 }
