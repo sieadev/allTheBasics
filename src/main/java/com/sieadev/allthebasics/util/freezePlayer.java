bcdfde9f948fcc2d6c82c374b156262bc4c22bdf
SIEA <69807609+sieadev@users.noreply.github.com>
2023-08-30 15:26:20 +0200

    Updated freezePlayer util
    Someone add: Save if player is fronzen in hashmap or something idk else remove it

--- src/main/java/com/sieadev/allthebasics/util/freezePlayer.java
+++ src/main/java/com/sieadev/allthebasics/util/freezePlayer.java
@@ -1,4 +1,30 @@
 package com.sieadev.allthebasics.util;
 
 public class freezePlayer {
+	public static void freezePlayer(Player p, String t){
+        if (!p.hasPermission("atb.freeze")){
+            p.sendMessage("§cYou don't have the required permissions to use this command.");
+            return;
+        }
+
+        if (t == null){
+            freeze(p);
+            p.sendMessage(ChatColor.YELLOW + "You healed yourself for §c" + Math.round(missingHealth) + "§c❤");
+            return;
+        }
+
+        Player target = getPlayerFromString.getPlayerFromString(p, t);
+
+        if (target == null){
+            return;
+        }
+
+        freeze(target);
+        p.sendMessage(ChatColor.YELLOW + "You froze §r" + target.getDisplayName());
+        target.sendMessage(p.getDisplayName() + ChatColor.YELLOW + " froze you");
+    }
+
+	public static void freeze(Player p){
+         //Implement Freeze Method
+    }
 }
