033fb3f825de0c5d12d5113bb55e13fd2c41fef3
SIEA <69807609+DrVosss@users.noreply.github.com>
2023-09-14 01:34:44 +0200

    removed unnecessary import statements

--- src/main/java/com/sieadev/allthebasics/listeners/PlayerChatEvent.java
+++ src/main/java/com/sieadev/allthebasics/listeners/PlayerChatEvent.java
@@ -1,18 +1,14 @@
 package com.sieadev.allthebasics.listeners;
 
-import org.bukkit.ChatColor;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
-import com.sieadev.allthebasics.util.chat.chatConverter;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import com.sieadev.allthebasics.util.text.chatMessageBuilder;
 
 public class PlayerChatEvent implements Listener {
 @EventHandler
     public void onPlayerChatEvent(AsyncPlayerChatEvent e) {
-        String message = chatConverter.all(e.getPlayer(), e.getMessage());
+        String message = chatMessageBuilder.all(e.getPlayer(), e.getMessage());
         e.setMessage(message);
     }
 }
