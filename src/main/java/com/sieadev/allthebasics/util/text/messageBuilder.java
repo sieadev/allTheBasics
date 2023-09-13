2dd6660f841a5ad24b5c7bd3bdeb047e9a1a37cc
SIEA <69807609+DrVosss@users.noreply.github.com>
2023-09-14 01:33:58 +0200

    added message Builder

---
+++ src/main/java/com/sieadev/allthebasics/util/text/messageBuilder.java
@@ -0,0 +1,8 @@
+package com.sieadev.allthebasics.util.text;
+
+public class messageBuilder {
+    public static String noPermission = "§cYou don't have the required permissions to use this command.";
+    public void loadMessagesFromLanguageFile(){
+
+    }
+}
