d351362a6957912a6431ec5ec5557131592f812a
SIEA <69807609+DrVosss@users.noreply.github.com>
2023-09-14 01:34:13 +0200

    added dm command

--- src/main/java/com/sieadev/allthebasics/commands/social/dm.java
+++ src/main/java/com/sieadev/allthebasics/commands/social/dm.java
@@ -1,4 +1,34 @@
 package com.sieadev.allthebasics.commands.social;
 
-public class dm {
+import com.sieadev.allthebasics.util.homeUtility;
+import com.sieadev.allthebasics.util.social.sendDirectMessage;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class dm implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (!(sender instanceof Player)){
+            sender.sendMessage("§cYou can not use this command in the Console.");
+            return true;
+        }
+        if (args.length > 2) {
+            return false;
+        }
+        Player p = (Player)sender;
+
+        //Credits to ChatGPT cause I was lazy as fk
+
+        // Concatenate all arguments after args[0] into a single string
+        StringBuilder messageBuilder = new StringBuilder();
+        for (int i = 1; i < args.length; i++) {
+            messageBuilder.append(args[i]).append(" ");
+        }
+        String message = messageBuilder.toString().trim();
+
+        sendDirectMessage.sendDirectMessage(p, args[0], message);
+        return true;
+    }
 }
