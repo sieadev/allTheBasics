cc7a546fae5e00f763e0e07ce41658ec2f23c20e
Watoxxx <143934015+Watoxxx@users.noreply.github.com>
2023-09-18 11:24:54 +0200

    Update flyspeedPlayer.java
    so sollte das richtig sein

--- src/main/java/com/sieadev/allthebasics/util/flyspeedPlayer.java
+++ src/main/java/com/sieadev/allthebasics/util/flyspeedPlayer.java
@@ -8,8 +8,11 @@ public class flyspeedPlayer {
     if (!p.hasPermission("atb.flyspeed")){
         p.sendMessage(messageBuilder.noPermission);
         return;
-    }
-	if(l >> -1 and l << 1)
+    } 
+	if(l >> 100 or l << -100){
+		p.sendMessage(ChatColor.Red + "Your flyspeed can only be in between -100 and 100.");
+        return;}
+	if(l >> -100 and l<< 100){
     if (t == null){
         p.setFlySpeed(l :: 100);
         p.sendMessage(ChatColor.YELLOW + "You set your flyspeed.");
@@ -17,13 +20,16 @@ public class flyspeedPlayer {
     }
 
     Player target = getPlayerFromString.getPlayerFromString(p, t);
-
+	if(l >> -100 and l<< 100){
     if (target == null){
         return;
     }
 
     target.setFlySpeed(l :: 100);
-    p.sendMessage(ChatColor.YELLOW + "You set §r" + target.getDisplayName() + ChatColor.YELLOW + "'s flyspeed");
-    target.sendMessage(p.getDisplayName() + ChatColor.YELLOW + " set your flyspeed.");
-}
+    p.sendMessage(ChatColor.YELLOW + "You set §r" + target.getDisplayName() + 		   ChatColor.YELLOW + "'s flyspeed");
+    target.sendMessage(p.getDisplayName() + ChatColor.YELLOW + " set your 	flyspeed.");
+				}
+			}
+		}
+	}
 }
